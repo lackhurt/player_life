c2857e19d2c9bc93bb513b8af649cee8537c1d6a
lackhurt <pangjn@yeah.net>
2015-05-10 17:56:01 +0800

    USER API ADDED

--- app/Services/API/UsersAPI.php
+++ app/Services/API/UsersAPI.php
@@ -1,4 +1,25 @@
 <?php namespace App\Services\API;
-class UserAPI {
 
+use Illuminate\Support\Facades\Validator;
+
+class UsersAPI {
+
+    public function isPhoneExists($phoneNo) {
+        return false;
+    }
+
+    public function isEmailExists($email) {
+        return false;
+    }
+
+    public function validate($userData) {
+        $result = Validator::make($userData, [
+            'name' => 'required',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($result->fails()) {
+            return $result->messages();
+        }
+    }
 }
