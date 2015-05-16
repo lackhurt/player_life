4a9426085d6cc1b61697badc838065bb99ddd954
spfroc <spf_roc@163.com>
2015-05-16 23:09:03 +0800

    用户帐号注册表单服务端验证规则

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,7 +27,7 @@
                 <div class="form-group">
                     <label class="col-md-4 control-label">短信激活码</label>
                     <div class="col-md-6">
-                        <input type="email" class="form-control" name="phone_identifying_code" value="">
+                        <input type="text" class="form-control" name="phone_identifying_code" value="">
                     </div>
                 </div>
 
