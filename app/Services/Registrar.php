ea5aa62141030387dc1c03fc1413110ce2facf7f
spfroc <spf_roc@163.com>
2015-05-16 23:07:28 +0800

    用户帐号注册表单服务端验证规则

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -1,6 +1,7 @@
 <?php namespace App\Services;
 
 use App\User;
+use Illuminate\Support\Facades\Session;
 use Validator;
 use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
 
@@ -14,8 +15,29 @@ class Registrar implements RegistrarContract {
 	 */
 	public function validator(array $data)
 	{
-		return Validator::make($data, [
-		]);
+
+        $rules = [
+            "phone" => ['required','unique:users', 'numeric', 'regex: /^(\+86)?((13[0-9])|(15[0-9])|(17[08])|(18[0-9]))\d{8}$/'],
+            "password" => 'required|confirmed',
+            "password_confirmation" => 'required',
+            "captcha_code" => 'required|in:'.Session::get('__captcha'),
+            "phone_identifying_code" => 'required',
+        ];
+
+        $messages = [
+            'phone.required' => '请输入手机号',
+            'password.required' => '请输入密码',
+            'password_confirmation.required' => '请确认密码',
+            'phone.regex' => '手机号格式不正确',
+            'captcha_code.require' => '请输入验证码',
+            'phone_identifying_code.require' => '请输入短信激活码',
+            'password.confirmed' => '两次输入密码不一致',
+            'captcha_code.in' => '验证码无效',
+
+        ];
+
+
+		return Validator::make($data, $rules, $messages);
 	}
 
 	/**
@@ -27,6 +49,7 @@ class Registrar implements RegistrarContract {
 	public function create(array $data)
 	{
 		return User::create([
+            'phone' => $data['phone'],
 			'password' => bcrypt($data['password']),
 		]);
 	}
