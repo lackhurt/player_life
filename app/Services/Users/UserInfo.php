5700d960b740a3db3550e8bd7c0fda0f250bcd10
spfroc <spf_roc@163.com>
2015-05-17 14:56:04 +0800

    用户信息service

---
+++ app/Services/Users/UserInfo.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 15-5-17
+ * Time: 下午1:52
+ */
+
+namespace App\Services\Users;
+
+use App\User;
+use Illuminate\Support\Facades\Session;
+use Validator;
+
+class UserInfo {
+
+    public function validatorBase(array $data) {
+        $rules = [
+            "phone" => ['required','unique:users', 'numeric', 'regex: /^(\+86)?((13[0-9])|(15[0-9])|(17[08])|(18[0-9]))\d{8}$/'],
+            "password" => 'required|confirmed',
+            "password_confirmation" => 'required',
+            "captcha_code" => 'required|in:'.Session::get('__captcha'),
+            "phone_identifying_code" => 'required',
+        ];
+
+        $messages = [
+            'phone.required' => '请输入手机号',
+            'password.required' => '请输入密码',
+            'password_confirmation.required' => '请确认密码',
+            'phone.regex' => '手机号格式不正确',
+            'captcha_code.required' => '请输入验证码',
+            'phone_identifying_code.required' => '请输入短信激活码',
+            'confirmed' => '两次输入密码不一致',
+            'captcha_code.in' => '验证码无效',
+
+        ];
+        return Validator::make($data, $rules, $messages);
+
+    }
+
+
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    public function create(array $data)
+    {
+        return User::create([
+            'phone' => $data['phone'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+
+
+} 
