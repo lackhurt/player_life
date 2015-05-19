eecc4b2f93509abc54fa8ea5d16a755063cec836
spfroc <spf_roc@163.com>
2015-05-19 23:30:56 +0800

    用户信息编辑控制器修改

--- app/Services/Users/UserInfo.php
+++ app/Services/Users/UserInfo.php
@@ -38,6 +38,30 @@ class UserInfo {
 
     }
 
+    public function validatorIdentify(array $data) {
+        $rules = [
+//            "phone" => ['required', 'numeric', 'regex: /^(\+86)?((13[0-9])|(15[0-9])|(17[08])|(18[0-9]))\d{8}$/'],
+//            "password" => 'required|confirmed',
+//            "password_confirmation" => 'required',
+//            "captcha_code" => 'required|in:'.Session::get('__captcha'),
+//            "phone_identifying_code" => 'required',
+        ];
+
+        $messages = [
+//            'phone.required' => '请输入手机号',
+//            'password.required' => '请输入密码',
+//            'password_confirmation.required' => '请确认密码',
+//            'phone.regex' => '手机号格式不正确',
+//            'captcha_code.required' => '请输入验证码',
+//            'phone_identifying_code.required' => '请输入短信激活码',
+//            'confirmed' => '两次输入密码不一致',
+//            'captcha_code.in' => '验证码无效',
+
+        ];
+        return Validator::make($data, $rules, $messages);
+
+    }
+
 
 
     /**
@@ -54,8 +78,8 @@ class UserInfo {
         ]);
     }
 
-    //用户信息编辑
-    public function update(array $data, $id) {
+    //用户基础信息编辑
+    public function updateBase(array $data, $id) {
         $user = User::find($id);
         $user->update([
             'birthday' => strtotime($data['birthday']),
@@ -68,6 +92,18 @@ class UserInfo {
         ], array());
 
     }
+
+    //用户基础信息编辑
+    public function updateIdentify(array $data, $id) {
+        $user = User::find($id);
+        $user->update([
+            'identify_card' => $data['identify_card'],
+            'name' => $data['name'],
+        ], array());
+
+    }
+
+
 
 
 } 
