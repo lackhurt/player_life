521323e40b5df24368e0ab5e343b3b075a4d75d2
chejv <307322148@qq.com>
2015-06-06 15:54:08 +0800

    添加联系方式

--- app/Services/Users/UserInfo.php
+++ app/Services/Users/UserInfo.php
@@ -62,6 +62,30 @@ class UserInfo {
 
     }
 
+    public function validatorContract(array $data) {
+        $rules = [
+//            "phone" => ['required', 'numeric', 'regex: /^(\+86)?((13[0-9])|(15[0-9])|(17[08])|(18[0-9]))\d{8}$/'],
+//            "password" => 'required|confirmed',
+//            "password_confirmation" => 'required',
+//            "captcha_code" => 'required|in:'.Session::get('__captcha'),
+//            "phone_identifying_code" => 'required',
+        ];
+
+        $messages = [
+//            'phone.required' => '请输入手机号',
+//            'password.required' => '请输入密码',
+//            'password_confirmation.required' => '请确认密码',
+//            'phone.regex' => '手机号格式不正确',
+//            'captcha_code.required' => '请输入验证码',
+//            'phone_identifying_code.required' => '请输入短信激活码',
+//            'confirmed' => '两次输入密码不一致',
+//            'captcha_code.in' => '验证码无效',
+
+        ];
+        return Validator::make($data, $rules, $messages);
+
+    }
+
 
 
     /**
@@ -99,6 +123,16 @@ class UserInfo {
         $user->update([
             'identify_card' => $data['identify_card'],
             'name' => $data['name'],
+        ], array());
+C
+    }
+    //用户联系信息编辑
+    public function updateContract(array $data, $id) {
+        $user = User::find($id);
+        $user->update([
+            'email' => $data['email'],
+            'qq' => $data['qq'],
+            'wechat' => $data['wehat'],
         ], array());
 
     }
---
+++ resources/views/user/info/contract.blade.php
@@ -0,0 +1,64 @@
+@extends('app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/user/info/contract') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">手机号</label>
+                    <div class="col-md-6">
+                        <label class="col-md-6 control-label">{{$user->phone}}</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">邮箱</label>
+                    <div class="col-md-6">
+                        <input type="email" required class="form-control" name="email" value="{{$user->email}}">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">QQ</label>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" name="qq" value="{{$user->qq}}">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">微信</label>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" name="weChat" value="{{$user->weChat}}">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">
+                            　提交　
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div id="example"></div>
+<script>
+
+
+
+</script>
+@endsection
