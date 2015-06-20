7d3c4ecb42de0e239f0e34d960ec31f2c77a9b9a
sunpengfei <sumpengfei@social-touch.com>
2015-06-20 20:07:04 +0800

    战队信息浏览控制器修改

--- app/Http/Controllers/Corp/InfoController.php
+++ app/Http/Controllers/Corp/InfoController.php
@@ -9,12 +9,18 @@
 namespace App\Http\Controllers\Corp;
 
 use App\Http\Controllers\Controller;
+use App\Services\Corp\Corp;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Guard;
 
 
 class InfoController extends Controller {
-    public function getIndex() {
+    public function getIndex(Request $request, Corp $corp, Guard $guard) {
+        $id = $request->get('id');
         return view('corp/info')->with([
-            'title' => '战队信息'
+            'title' => '战队信息',
+            'corp' => $corp->getCorpInfo($id),
+            'isBelongCorp' => $corp->isBelongToCorp($corp, $guard)
         ]);
     }
 }
